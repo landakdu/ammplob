b2c284a00a1c2f817c5e41816bb57aa923a663e5
judisuhu1-ux <judisuhu1@gmail.com>
2026-03-18 10:03:46 +0530

    Update save.js

--- save.js
+++ save.js
@@ -1,5 +1,6 @@
 document.getElementById('yourform').submit();
 function link() {location.replace("https://www.senisastra.com/");}
+function linkbtisport() {location.replace("https://www.senisastra.com/");}
 function linksabasport() {location.replace("https://www.senisastra.com/");}
 function linkqqgowin() {location.replace("https://www.senisastra.com/");}
 function linkqqbos99() {location.replace("https://www.senisastra.com/");}
