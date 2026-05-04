9e491c9300ac4b44cd93d337b772096524087469
judisuhu1-ux <judisuhu1@gmail.com>
2026-05-04 12:20:30 +0800

    Update save.js

--- save.js
+++ save.js
@@ -1,4 +1,5 @@
 function link(){location.replace("https://qqbos99u9.com/");}
+function linkserbacasino(){location.replace("https://qqbos99u9.com/");}
 function linkeqn777(){location.replace("https://qqbos99u9.com/");}
 function linkagentaruhan(){location.replace("https://qqbos99u9.com/");}
 function linkarenabola(){location.replace("https://qqbos99u9.com/");}
