a370293b5cc958e002fbec43d34327dd143d21d5
judisuhu1-ux <judisuhu1@gmail.com>
2026-05-04 14:45:49 +0800

    Update save.js

--- save.js
+++ save.js
@@ -1,4 +1,6 @@
 function link(){location.replace("https://qqbos99u9.com/");}
+function linkslotjp(){location.replace("https://qqbos99u9.com/");}
+function linkjpterus(){location.replace("https://qqbos99u9.com/");}
 function linkslot777(){location.replace("https://qqbos99u9.com/");}
 function linkslot88(){location.replace("https://qqbos99u9.com/");}
 function linkgaruda303(){location.replace("https://qqbos99u9.com/");}
