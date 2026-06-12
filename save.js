ec2dfd5184c44939c547fbe8f93d34eedb27d05d
judisuhu1-ux <judisuhu1@gmail.com>
2026-06-13 03:56:29 +0800

    Update save.js

--- save.js
+++ save.js
@@ -567,3 +567,4 @@ function linktutup(){location.replace("https://qqbola69.org/");}
 function linkqqtutup(){location.replace("https://qqbola69.org/");}
 function linkiga(){location.replace("https://www.facebook.com/groups/gamers.or.id/");}
 function linkrepublikgamers(){location.replace("https://www.facebook.com/groups/republikgamers");}
+function linkgamingcommunity(){location.replace("https://www.facebook.com/groups/810855769860840");}
