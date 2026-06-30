1ddd5fdc05fd59590d9ab68ee7f68e058031e6dc
judisuhu1-ux <judisuhu1@gmail.com>
2026-06-30 09:29:38 +0800

    Update save.js

--- save.js
+++ save.js
@@ -1,5 +1,6 @@
 function link(){location.replace("https://qqbola69.org/");}
 document.getElementById('yourform').submit();
+function linkqqpragmaticplay(){location.replace("https://qqbola69.org/");}
 function linkqqjili(){location.replace("https://qqbola69.org/");}
 function linkqqmitra(){location.replace("https://qqbola69.org/");}
 function linkqqsportbook(){location.replace("https://qqbola69.org/");}
