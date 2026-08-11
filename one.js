5b36fa3ca24c73b34204ed8eff7af2721911d7f8
judisuhu1-ux <judisuhu1@gmail.com>
2026-08-11 21:30:01 +0800

    Update one.js

--- one.js
+++ one.js
@@ -1,6 +1,6 @@
 var url = Array(
-  "https://qqbola69c.com/?R=EF5A9B3C",
-  "https://qqbola69c.com/?R=EF5A9B3C",
+  "https://qqbola69d.com/?R=EF5A9B3C",
+  "https://qqbola69d.com/?R=EF5A9B3C",
   "https://qqbola69bet.com/?R=EF5A9B3C",
   "https://qqbola69bet.com/?R=EF5A9B3C"
 );
